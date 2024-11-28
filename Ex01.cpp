b63c1b7a4ffb976384fc9ee049dbf9999e76bd1a
DangToan06 <dangtoan.2k6@gmail.com>
2024-11-28 11:36:03 +0700

    Update Ex01.cpp

--- Ex01.cpp
+++ Ex01.cpp
@@ -2,19 +2,9 @@
 
 int main(){
 	int arr[5] = {1,2,3,4,5};
-	
 	int size = sizeof(arr)/sizeof(arr[0]);
-	
-	printf("Gia tri vi tri 0 la: %d\n", arr[0]);
-	printf("Gia tri vi tri 1 la: %d\n", arr[1]);
-	printf("Gia tri vi tri 2 la: %d\n", arr[2]);
-	printf("Gia tri vi tri 3 la: %d\n", arr[3]);
-	printf("Gia tri vi tri 4 la: %d\n", arr[4]);
-	printf("Do dai cua mang la: %d", size);
-	
-	
-	
-
+	for(int i = 0; i < 5; i++){
+		printf("\nPhan tu thu %d la: %d", i, arr[i]);
+	}
 	return 0;
 }
-
